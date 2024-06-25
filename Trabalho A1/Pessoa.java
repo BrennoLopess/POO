8b0fe78e6c97aea45f0722dfe5439054bdfb52f4
Brenno de Aguiar Pinheiro Lopes <133117780+BrennoLopess@users.noreply.github.com>
2024-06-25 10:50:22 -0300

    Create Pessoa.java

---
+++ Trabalho A1/Pessoa.java
@@ -0,0 +1,13 @@
+package models;
+
+public abstract class Pessoa {
+    protected String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
