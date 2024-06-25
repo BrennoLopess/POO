ca1a8bf26e007af4f88106bde909a62b5d41a26f
Brenno de Aguiar Pinheiro Lopes <133117780+BrennoLopess@users.noreply.github.com>
2024-06-25 10:50:38 -0300

    Create Aluno.java

---
+++ Trabalho A1/Aluno.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Aluno extends Pessoa {
+    private String curso;
+
+    public Aluno(String nome, String curso) {
+        super(nome);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+}
